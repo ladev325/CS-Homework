7e95a2c93c4a877829aaa2b582fae6c86606c5e8
ladev325 <vlad2008artemov@gmail.com>
2025-05-23 14:53:11 +0300

    dthtdhxdftjftjfty

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -1,118 +1,137 @@
 ﻿using System;
-using System.Linq;
 
 namespace Example_1
 {
-    enum EnUm
-    {
-        a = 1,
-        b = 2,
-        c = 3
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            //14
-            String str = "aboba";
-            bool palindrom = true;
-            for (int i = 0; i < str.Length / 2; i++)
+            //1
+            int height = 10;
+            for (int i = 0; i < height; i++)
             {
-                if (str[i] != str[str.Length - i - 1])
-                    palindrom = false;
-            }
-            if (palindrom) Console.WriteLine($"{str} is a palindrom");
-            else Console.WriteLine($"{str} is NOT a palindrom");
-
-            //15
-            int[] arr = { 0, 1, 1, 2, 2, 3, 3, 4, 4 };
-            List<int> numbers = new List<int> { }; // вектор для c#
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (!numbers.Contains(arr[i]))
-                    numbers.Add(arr[i]);
-            }
-            arr = numbers.ToArray();
-            Console.WriteLine(string.Join(", ", arr));
-
-            //16
-            int sum = 0;
-            foreach (int val in Enum.GetValues(typeof(EnUm)))
-            {
-                sum += val;
-            }
-            Console.WriteLine($"enum sum: {sum}");
-            //17
-            int a = 6;
-            int b = 66;
-            int nsd = 0;
-
-            for (int i = Math.Min(a, b); i > 0; i--)
-            {
-                if (a % i == 0 && b % i == 0)
+                for (int j = 0; j < height - i - 1; j++)
                 {
-                    nsd = i;
-                    break;
+                    Console.Write(" ");
                 }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
             }
 
-            Console.WriteLine($"nsd: {nsd}");
-
-            //18
-            int[] arr1 = { 0, 1, 2, 3 };
-            int[] arr2 = { 0, 1, 2, -1 };
-            if(arr1.SequenceEqual(arr2)) Console.WriteLine($"arr1 == arr2");
-            else Console.WriteLine($"arr1 != arr2");
-
-            //19
-            int[] evenodd = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            List<int> even = new List<int> { };
-            List<int> odd = new List<int> { };
-            for(int i = 0; i < evenodd.Length; i ++)
-            {
-                if (evenodd[i] % 2 == 0) even.Add(evenodd[i]);
-                else odd.Add(evenodd[i]);
-            }
-            Console.WriteLine();
-            Console.WriteLine($"even: {string.Join(", ", even)}");
-            Console.WriteLine($"odd: {string.Join(", ", odd)}");
             Console.WriteLine();
 
-            //20
-            String str2 = "hello this string has This word";
-            bool has_cap_letter = false;
-            for(int i = 0; i < str2.Length; i ++) {
-                if (char.IsUpper(str2[i]) && (i == 0 || str2[i - 1] == ' '))
+            //2
+            bool state = false;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
                 {
-                    has_cap_letter = true;
-                    break;
+                    state = !state;
+                    if (state) Console.Write("X ");
+                    else Console.Write("O ");
+
+                }
+                state = !state;
+                Console.WriteLine("");
+            }
+            Console.WriteLine();
+
+            //3
+            for (int i = 0; i < 20; i +=2)
+            {
+                for(int j = 0; j < i; j ++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //4
+
+            for (int i = 0; i < 10; i ++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(i == j || 10 - i == j + 1) Console.Write("+ ");
+                    else Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //5
+            /*
+            Random rnd = new Random();
+
+            while (true)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < 30; i++)
+                {
+                    for (int j = 0; j < 30; j++)
+                    {
+                        if((j >= 15 && j <= 24 && i >= 12 && i <= 14) || (j == 25 && i == 13)) Console.Write("#");
+                        else if(rnd.Next(50) == 1) Console.Write("*");
+                        else Console.Write(" ");
+                    }
+                    Console.WriteLine();
                 }
             }
-            if (has_cap_letter) Console.WriteLine($"arr2 has a cap letter");
-            else Console.WriteLine($"arr2 has no cap letters");
+            */
 
-            //21
-            Console.Write("Enter int number: ");
-            String num = Console.ReadLine();
-            int number = int.Parse(num);
-            String str_num = "";
-
-            if(number < 0) str_num += '-';
-            number = Math.Abs(number);
-
-            if (number == 0) str_num += "0";
-
-            string digits = "";
-            while (number > 0)
+            //6
+            for (int i = 0; i < 10; i++)
             {
-                int digit = number % 10;
-                number /= 10;
-                digits = (char)('0' + digit) + digits;
+                for (int j = 0; j < 10; j++)
+                {
+                    if(j >= 3 && j <= 6 && i >= 3 && i <= 6) Console.Write("o ");
+                    else if (i == j || 10 - i == j + 1) Console.Write("x ");
+                    else Console.Write("  ");
+                }
+                Console.WriteLine();
             }
+            Console.WriteLine();
 
-            str_num += digits;
+            //7
+            /*Random rnd2 = new Random();
 
-            Console.WriteLine($"string: {str_num}");
+            while (true)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < 30; i++)
+                {
+                    for (int j = 0; j < 30; j++)
+                    {
+                        if (rnd2.Next(50) == 1) Console.Write("|");
+                        else Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }*/
+
+            //8
+            Console.WriteLine("Enter height: ");
+            int height1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < height1 + 1; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("^");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
 
         }
     }
