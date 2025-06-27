727bd2d7db1c65ada63d493fcff4b98115cb2a99
ladev325 <vlad2008artemov@gmail.com>
2025-06-27 15:42:05 +0300

    rgdrgdrgdrg

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -5,12 +5,38 @@ namespace Example_1
 {
     internal class Program
     {
+        static void InfoElf(Elf elf)
+        {
+            elf.Info();
+        }
+
+        static void ElfAttack(Elf elf)
+        {
+            elf.Attack();
+        }
+
+        static void ElfSound(Elf elf)
+        {
+            elf.Sound();
+        }
+
         static void Main(string[] args)
         {
+            // ------ я вже зробив що було в завданні тому просто додав поліморфні методи
+
             TempleKnight temple_knight = new TempleKnight(100, "Weapon", "Magic", "Karma", "AttackMethod");
-            temple_knight.Info();
-            temple_knight.Attack();
-            temple_knight.Sound();
+
+            // взагалі з іншої гілки успадкування
+            EvasSaint evas_saint = new EvasSaint(1000, "Weaponnnn", "Total magic", "Karma", "Magic method");
+
+
+            InfoElf(temple_knight);
+            ElfAttack(temple_knight);
+            ElfSound(temple_knight);
+
+            InfoElf(evas_saint);
+            ElfAttack(evas_saint);
+            ElfSound(evas_saint);
         }
     }
 }
